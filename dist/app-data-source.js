fa46397f63934d5c87be35f4b1358dd4526239d3
Dbachelor <84332279+Dbachelor@users.noreply.github.com>
2023-08-05 08:44:29 +0100

    Update app-data-source.js

--- dist/app-data-source.js
+++ dist/app-data-source.js
@@ -1,11 +1,13 @@
+import { defaults } from "pg";
 import { DataSource } from "typeorm";
+
 export const appDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "mysql",
-    database: "wallet",
+    password: defaults.password,
+    database: "wallet_c5h8",
     entities: ["./dist/entity/*.js"],
     logging: true,
     synchronize: true,
