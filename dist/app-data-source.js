b2f4bce3ce77c0c3c719caec19cfefb177a99bab
Dbachelor <84332279+Dbachelor@users.noreply.github.com>
2023-08-05 09:41:55 +0100

    Update app-data-source.js

--- dist/app-data-source.js
+++ dist/app-data-source.js
@@ -4,7 +4,7 @@ import { defaults } from "./config.js";
 
 export const appDataSource = new DataSource({
     type: "postgres",
-    host: "postgres://root:4eCyXTpCEEaZsdjN59hi3Koinx0ewI11@dpg-cj6vltsl975s73cmnmeg-a/wallet_c5h8",
+    host: "dpg-cj6vltsl975s73cmnmeg-a",
     port: 3306,
     username: "root",
     password: defaults.password,
