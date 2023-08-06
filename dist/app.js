e3e24ef6c52d9d8d7fdab01c51d43b1186eb7343
Dbachelor <84332279+Dbachelor@users.noreply.github.com>
2023-08-06 08:29:00 +0100

    added cors

--- dist/app.js
+++ dist/app.js
@@ -4,6 +4,7 @@ import { appDataSource } from "./app-data-source.js";
 import { UserRouter } from "./Route/user-route.js";
 import { defaults } from "./config.js";
 import { TransactionRouter } from "./Route/transaction-route.js";
+import cors from 'cors'
 const { register, login } = new AuthController();
 const {port} = defaults;
 await appDataSource
@@ -18,11 +19,10 @@ await appDataSource
 
 // create and setup express app
 const app = express();
-app.use(express.json());
 app.use(cors({
-    origin: '*',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: '*'
   }))
+app.use(express.json());
 // register routes
 app.get('/', function(req, res){
     res.send("wallet app");
